8f85581c2cb6021990b6086097421611dc531aaf
Justonchari <justinonchari@gmail.com>
2023-06-22 18:42:32 -0700

    do nothing

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+  *f_nop- this func does nothing
+  *@head: parameter with stack head
+  *@counter: line_number parameter
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
